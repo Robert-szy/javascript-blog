faec2014657235349f235d85dc458f7a45748402
Robert Szyszka <robert.szyszka@outlook.com>
2021-01-20 23:53:43 +0100

    Add editorconfig

--- js/script.js
+++ js/script.js
@@ -2,34 +2,34 @@
 const titleClickHandler = function(event){
     console.log('Link was clicked!');
     console.log('event:', event);
-  
+
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
-    
+
     /* [DONE] add class 'active' to the clicked link */
     event.preventDefault();
     const clickedElement = this;
     console.log('clickedElement:', clickedElement);
     console.log('clickedElement (with plus):' + clickedElement);
     clickedElement.classList.add('active');
-  
+
     /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .active');
     for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
     }
-  
+
     /* [DONE] get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute('href');
     console.log('articleSelector:', articleSelector);
-  
+
     /* [DONE] find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
     console.log('targetArticle:', targetArticle);
-  
+
     /* [DONE] add class 'active' to the correct article */
     targetArticle.classList.add('active');
 
@@ -70,25 +70,25 @@ const generateTitleLinks = function(){
     /* [DONE] remove links from left panel*/
     const titleList = document.querySelector(optTitleListSelector)
     titleList.innerHTML = '';
-    
+
     /* [DONE] for each article: */
     const articles = document.querySelectorAll(optArticleSelector);
     console.log ('articles: ', articles);
-    let html = '';   
+    let html = '';
     for(let article of articles){
-    
+
         /* [DONE] find article id */
         const articleId = article.getAttribute('id');
         console.log ('article: ', article);
         console.log ('id: ', articleId);
-                        
+
         /* [DONE] find article title */
         const articleTitle = article.querySelector(optTitleSelector).innerHTML;
         console.log ('articleTitle: ', articleTitle);
 
         const articleTitle1 = document.querySelector(optTitleSelector).innerHTML;
         console.log ('articleTitle1: ', articleTitle1);
-        
+
         /* [DONE] generate HTML with title and id */
         const linkHTML = '<li><a href="#'+ articleId +'"><span>'+ articleTitle +'</span></a></li>';
         console.log ('linkHTML: ', linkHTML);
@@ -96,7 +96,7 @@ const generateTitleLinks = function(){
         /* [DONE] insert HTML into link list */
         //titleList.innerHTML = titleList.innerHTML + linkHTML;
         //titleList.insertAdjacentHTML ("beforeend", linkHTML);
-        
+
         html = html + linkHTML;
         console.log('html', html)
     }
@@ -108,7 +108,7 @@ const generateTitleLinks = function(){
       link.addEventListener('click', titleClickHandler);
     }
 }
-  
+
 generateTitleLinks ();
 
 }
